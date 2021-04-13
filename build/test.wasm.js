d17d90fe57c415f9fb187f365632bc642acd1edd
zonghaishang <yiji@apache.org>
2021-04-13 13:55:42 +0800

    add maven plugin.

--- build/test.wasm.js
+++
@@ -1,4 +0,0 @@
-'use strict';var wasmImports = {
-
-};
-if (typeof module !== 'undefined') module.exports = wasmImports;
--- src/main/java/com/alipay/wasm/WasmFunctions.java
+++ src/main/java/com/alipay/wasm/WasmFunctions.java
@@ -1,9 +1,20 @@
 package com.alipay.wasm;
 
+import de.mirkosertic.bytecoder.api.Export;
+import de.mirkosertic.bytecoder.api.Import;
+
 /**
  * https://mirkosertic.github.io/Bytecoder/chapter-1/page-1-a/
  */
 public class WasmFunctions {
+
+    @Import(module = "", name = "begin")
+    public static native void statsBegin();
+
+    @Export("hello")
+    public static int hello() {
+        return 43;
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
